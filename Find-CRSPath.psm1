7a218e6faa458dbd04242cee878edce5cad11447
Ben Therien <cs135cid@yahoo.com>
2019-08-30 18:42:08 -0400

    Create Find-CRSPath.psm1

---
+++ Find-CRSPath.psm1
@@ -0,0 +1,11 @@
+﻿Function Find-CRSPath {
+    IF ($host.name -eq 'Windows Powershell ISE Host') { #If the script is being run in ISE
+        $Global:CRSPath = $psISE.CurrentFile.FullPath | Split-Path}  #Get the Directory where this script is saved, and then chop off the file name
+        
+
+    IF ($host.name -eq 'ConsoleHost') { #If the Script is being run normally
+        $Global:CRSPath = Split-Path $PSCommandPath}
+            
+    #Error Checking
+    IF ($CRSPath -eq "" -or !(Test-Path variable:global:CRSPath)) {Messages -Msgtype Error -MsgLevel Fatal -Location Function-CRSPath  -Message "Path of current running script not found" -Impact "Script will NOT be able to find Modules, cannot continue."}
+}
